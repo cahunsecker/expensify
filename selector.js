0c3d7e3d5dfef7e5a722ad9143f28cf611ec4b1e
cahunsecker <55468391+cahunsecker@users.noreply.github.com>
2020-07-13 16:05:19 -0500

    Add files via upload
    This is the right selector file

--- selector.js
+++ selector.js
@@ -1,18 +1,15 @@
 const getVisibleExpenses = (expenses, {text, startDate, endDate}) => {
-    console.log(text);
 
-    expenses.filter((expense) => (console.log(expense.note)))
+    return expenses.filter((expense) => {
+        console.log(expense.note);
 
-    // return expenses.filter((expense) => {
-    //     console.log(expense.note);
-
-    //     // const textMatch= expense.note.toLowerCase().includes(text.toLowerCase());
-    //     // const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate ;
-    //     // const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
+        const textMatch= expense.note.toLowerCase().includes(text.toLowerCase());
+        const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate ;
+        const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
 
 
-    //     // return textMatch && startDateMatch && endDateMatch;
-    // })
+        return textMatch && startDateMatch && endDateMatch;
+    })
    
 };
 export default getVisibleExpenses;
